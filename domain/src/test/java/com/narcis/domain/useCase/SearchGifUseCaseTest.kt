5a8fab4213fe5c4d422c9635a8a3064daa6b9445
narges-dpr <nargesdeypir@gmail.com>
2023-12-05 16:16:23 +0330

    test revisions - domain layer - search gif useCase

--- domain/src/test/java/com/narcis/domain/useCase/SearchGifUseCaseTest.kt
+++ domain/src/test/java/com/narcis/domain/useCase/SearchGifUseCaseTest.kt
@@ -1,24 +1,22 @@
 package com.narcis.domain.useCase
 
-import com.narcis.data.api.GiphyApi
 import com.narcis.data.model.search.SearchGif
 import com.narcis.data.repository.GifRepository
-import com.narcis.data.repository.GifRepositoryImpl
 import com.narcis.domain.common.Result
 import com.narcis.domain.di.DefaultDispatcherProvider
-import com.narcis.domain.model.SearchGifResult
+import com.narcis.domain.mapper.mapToSearchGif
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.impl.annotations.MockK
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
-import timber.log.Timber
-import kotlin.test.assertEquals
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import kotlin.test.assertTrue
 
 class SearchGifUseCaseTest {
-    @MockK
-    private lateinit var giphyApi: GiphyApi
 
     @MockK
     private lateinit var gifRepository: GifRepository
@@ -31,51 +29,38 @@ class SearchGifUseCaseTest {
     fun setUp() {
         MockKAnnotations.init(this)
         dispatcherProvider = DefaultDispatcherProvider()
-        gifRepository = GifRepositoryImpl(giphyApi = giphyApi)
         searchGifUseCase = SearchGifUseCase(gifRepository, dispatcherProvider)
     }
 
     @Test
-    fun `test executing search gif useCase retuning a result of searched gifs`() = runBlocking {
-        val searchGifResults = listOf(
-            SearchGifResult(
-                id = "1",
-                url = "https://giphy.com/gifs/AgenceLusso-football-soccer-foot-6mm6mUUExaAecMAvMq",
-                downSizedUrl = "https://media3.giphy.com/media/6mm6mUUExaAecMAvMq/giphy.gif?cid=6df7b0fe2zhjvlxhkznbwoknmh3f5on4tvupzp2hs9hbib47&ep=v1_gifs_search&rid=giphy.gif&ct=g",
-                title = "sample gif_title",
-                rating = "pg-15"
-            )
-        )
-        val expectedRandomGifResult = Result.Success(searchGifResults)
-
+    fun `invoke searchGif use case with success response emit loading and returns success`() = runTest {
+        val query = ""
         val searchGifs = listOf(
             SearchGif(
                 id = "1",
                 url = "https://giphy.com/gifs/AgenceLusso-football-soccer-foot-6mm6mUUExaAecMAvMq",
                 downSizedUrl = "https://media3.giphy.com/media/6mm6mUUExaAecMAvMq/giphy.gif?cid=6df7b0fe2zhjvlxhkznbwoknmh3f5on4tvupzp2hs9hbib47&ep=v1_gifs_search&rid=giphy.gif&ct=g",
                 title = "sample gif_title",
-                rating = "pg-15"
-            )
+                rating = "pg-15",
+            ),
         )
-        val query = "fr"
 
         coEvery { gifRepository.searchGif(query = query) } returns searchGifs
-        val result = searchGifUseCase(parameters = query)
-        result.collect { rstl ->
-            when (rstl) {
-                is Result.Error -> {
-                    Timber.e("Test Error", rstl.exception.toString())
-                }
+        val result = searchGifUseCase(parameters = query).toList()
 
-                Result.Loading -> {
-                    assertEquals(Result.Loading, rstl)
-                }
+        assertTrue(result[0] is Result.Loading)
+        val successResult = result[1] as Result.Success
+        expectThat(successResult.data).isEqualTo(searchGifs.map { it.mapToSearchGif() })
+    }
 
-                is Result.Success -> {
-                    assertEquals(expectedRandomGifResult.data, rstl.data)
-                }
-            }
-
-        }
+    @Test
+    fun `invoke search use case with exception response emit loading then return error on exception`() = runTest {
+        val query = ""
+        val exception = Exception("Error")
+        coEvery { gifRepository.searchGif(query) } throws exception
+        val results = searchGifUseCase(query).toList()
+        assertTrue(results[0] is Result.Loading)
+        val errorResult = results[1] as Result.Error
+        expectThat(exception).isEqualTo(errorResult.exception)
     }
 }
